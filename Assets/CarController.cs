d85c8b3b661ce730fdd6b07128d392aa83ef92b5
AnshMittal1811 <anshm18111996@gmail.com>
2019-12-16 00:04:17 +0530

    Ground Test

---
+++ Assets/CarController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private bool soundPlayed = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!soundPlayed && transform.localPosition.y < 0.05f)
+        {
+            soundPlayed = true;
+            StartCoroutine(DelayPlaySound());
+        }
+    }
+
+    public void MoveCar()
+    {
+        transform.localPosition += new Vector3(0, 10, 0);
+        transform.eulerAngles += new Vector3(5, 20, 5);
+        soundPlayed = false;
+
+    }
+
+    IEnumerator DelayPlaySound()
+    {
+        yield return new WaitForSeconds(.2f);
+        GetComponent<AudioSource>().Play();
+    }
+}
---
+++ Assets/DeployStageOnce.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+
+public class DeployStageOnce : MonoBehaviour
+{
+    public GameObject AnchorStage;
+    private PositionalDeviceTracker _deviceTracker;
+    private GameObject _previousAnchor;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        if(AnchorStage == null)
+        {
+            Debug.Log("AnchorStage must be specified");
+            return;
+        }
+        AnchorStage.SetActive(false);
+    }
+
+    public void Awake()
+    {
+        VuforiaARController.Instance.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+    }
+
+    public void OnDestroy()
+    {
+        VuforiaARController.Instance.UnregisterVuforiaStartedCallback(OnVuforiaStarted);
+    }
+
+    private void OnVuforiaStarted()
+    {
+        _deviceTracker = TrackerManager.Instance.GetTracker<PositionalDeviceTracker>();
+    }
+
+    public void OnInteractiveHitTest(HitTestResult result)
+    {
+        if(result == null || AnchorStage == null)
+        {
+            Debug.LogWarning("Hit test is invalid or AnchorStage not set");
+            return;
+        }
+
+        // same anchor code from before
+
+        var anchor = _deviceTracker.CreatePlaneAnchor(Guid.NewGuid().ToString(), result);
+
+        // but now the anchor doesn't create a GameObject, so we will have to with the HitTestResult position and rotation values
+
+        GameObject anchorGO = new GameObject();
+        anchorGO.transform.position = result.Position;
+        anchorGO.transform.rotation = result.Rotation;
+
+        // Parent the stage to the new GameObject like you would have the anchor before
+
+        if (anchor != null)
+        {
+            AnchorStage.transform.parent = anchorGO.transform;
+            AnchorStage.transform.localPosition = Vector3.zero;
+            AnchorStage.transform.localRotation = Quaternion.identity;
+            AnchorStage.SetActive(true);
+        }
+        // Clean up
+
+        if (_previousAnchor != null)
+        {
+            Destroy(_previousAnchor);
+        }
+
+        // Save it
+        _previousAnchor = anchorGO;
+    }
+    // Update is called once per frame
+
+}
